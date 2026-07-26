94333af05a90c3828e590cfa29839fe13c28cf00
DenisReaver <145904927+DenisReaver@users.noreply.github.com>
2026-07-26 16:29:51 +0300

    Update wagmi.ts

--- wagmi.ts
+++ wagmi.ts
@@ -25,7 +25,7 @@ export const config = getDefaultConfig({
   projectId: '6cd43074381dca42cc0f029143e150c8',
   chains: [mainnet, base, optimism, arbitrum, arcMainnet],
   transports: {
-    [mainnet.id]: http('https://eth.llamarpc.com'),
+    [mainnet.id]: http('https://rpc.ankr.com/eth'),
     [base.id]: http('https://mainnet.base.org'),
     [optimism.id]: http('https://mainnet.optimism.io'),
     [arbitrum.id]: http('https://arb1.arbitrum.io/rpc'),
